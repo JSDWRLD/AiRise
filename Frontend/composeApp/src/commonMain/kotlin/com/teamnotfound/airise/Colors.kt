94d85a439f295075e75bad9844cffa943b73d699
Octavio <03hoctavio@gmail.com>
2025-03-15 11:12:34 -0700

    Colors file for commonly used colors

---
+++ Frontend/composeApp/src/commonMain/kotlin/com/teamnotfound/airise/Colors.kt
@@ -0,0 +1,9 @@
+package com.teamnotfound.airise
+
+import androidx.compose.ui.graphics.Color
+
+val Orange = Color(0xFFCE5100)
+val White = Color(0xFFFFFFFF)
+val Silver = Color(0xFFB5B0B3)
+val DeepBlue = Color(0xFF21565C)
+val Transparent = Color(0x00000000)
