5d89ef64fd67f5f07276b02e2caa49b557b42c1e
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2025-03-15 12:55:32 -0700

    chore: delete duplicate files, added username to UserAuth

--- Frontend/composeApp/src/commonMain/kotlin/com/teamnotfound/airise/Platform.kt
+++
@@ -1,9 +0,0 @@
-package com.teamnotfound.airise
-
-interface Platform {
-    val name: String
-}
-
-// expect declares a function that can be used in common
-// that is implemented different in ios and android.
-expect fun getPlatform(): Platform
--- Frontend/composeApp/src/commonMain/kotlin/com/teamnotfound/airise/serializable/UserOnboarding.kt
+++ Frontend/composeApp/src/commonMain/kotlin/com/teamnotfound/airise/serializable/UserOnboarding.kt
@@ -1,4 +1,4 @@
-package com.teamnotfound.airise
+package com.teamnotfound.airise.serializable
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
