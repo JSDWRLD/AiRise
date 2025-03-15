1e5f21c5361e2a26faf197f7903e3b60b234aaa5
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2025-03-15 07:09:18 -0700

    add: mongo model

---
+++ Backend/Model/User.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AiRise.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("username")]
+    public string Username { get; set; }
+
+    [BsonElement("password")] // TODO: Hash this with bcrypt later on
+    public string Password { get; set; }
+
+    [BsonElement("email")]
+    public string Email { get; set; }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,41 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace AiRise;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.MapOpenApi();
-}
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+        builder.Services.AddControllers();
+        // FOR DEVELOPMENT API TESTING
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(); 
+        //
+        var app = builder.Build();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+        // Configure the HTTP request pipeline.
+        // ONLY DURING DEVELOPMENT
+        if (app.Environment.IsDevelopment()) 
+        {
+            app.UseSwagger(); 
+            app.UseSwaggerUI(); 
+        }
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        app.UseHttpsRedirection();
+        app.MapControllers();
+        app.Run();
+    }
 }
