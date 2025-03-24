1831609b97e652412e5ec92cb1acd0fb6c0d7fc1
Alec Ferguson <alecferguson@csus.edu>
2025-03-24 15:13:57 -0700

    Fixed main view controller for ios testing.

--- Frontend/composeApp/src/iosMain/kotlin/com/teamnotfound/airise/MainViewController.kt
+++ Frontend/composeApp/src/iosMain/kotlin/com/teamnotfound/airise/MainViewController.kt
@@ -7,10 +7,12 @@ import androidx.compose.ui.window.ComposeUIViewController
 import com.teamnotfound.airise.network.UserClient
 import com.teamnotfound.airise.network.createHttpClient
 import io.ktor.client.engine.darwin.Darwin
+import com.khealth.KHealth
+import com.teamnotfound.airise.network.AppContainer
 
 fun MainViewController() = ComposeUIViewController {
     val platformConfig = defaultPlatformConfiguration()
-
+    val kHealth = KHealth()
     CompositionLocalProvider(
         LocalDensity provides platformConfig.density
     ) {
@@ -18,7 +20,8 @@ fun MainViewController() = ComposeUIViewController {
                 container = AppContainer(
                     userClient = remember {
                         UserClient(createHttpClient(Darwin.create()))
-                    }
+                    },
+                    kHealth = kHealth
                 )
             )
     }
