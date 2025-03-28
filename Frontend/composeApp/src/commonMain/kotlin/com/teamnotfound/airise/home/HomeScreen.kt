255827568d202838904138940a460388cfc57cbc
Octavio <03hoctavio@gmail.com>
2025-03-28 16:57:27 -0700

    cleaning up imports

--- Frontend/composeApp/src/commonMain/kotlin/com/teamnotfound/airise/home/HomeScreen.kt
+++ Frontend/composeApp/src/commonMain/kotlin/com/teamnotfound/airise/home/HomeScreen.kt
@@ -1,46 +1,19 @@
 package com.teamnotfound.airise.home
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.teamnotfound.airise.util.White
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.draw.clip
-import androidx.compose.material.icons.Icons
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.LocalFireDepartment
-import androidx.compose.material.icons.automirrored.outlined.DirectionsRun
-import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.datetime.Clock
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.minus
-import kotlinx.datetime.*
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.Fill
-import com.teamnotfound.airise.util.DeepBlue
-import com.teamnotfound.airise.util.Silver
-import androidx.navigation.NavController
 import androidx.compose.material.Scaffold
 import androidx.navigation.compose.rememberNavController
-import com.teamnotfound.airise.data.serializable.HealthData
 import com.teamnotfound.airise.navigationBar.BottomNavigationBar
 
 @Composable
