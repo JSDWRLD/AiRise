4892e3d7a52d388170882219861c491272634f33
kosmckid <kosmckid@laptop>
2025-03-28 21:08:41 -0700

    fix formating for dob/height/weight/ai personality pages

--- Frontend/composeApp/src/commonMain/kotlin/com/teamnotfound/airise/auth/onboarding/onboardingQuestions/OnboardingScreen.kt
+++ Frontend/composeApp/src/commonMain/kotlin/com/teamnotfound/airise/auth/onboarding/onboardingQuestions/OnboardingScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
@@ -15,7 +17,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.*
 import androidx.navigation.NavHostController
 import com.teamnotfound.airise.auth.onboarding.ThankYouScreen
@@ -45,36 +49,54 @@ fun NavigateQuestions(navController: NavHostController, newUser: UserData){
         composable(OnboardingScreens.DietaryGoal.route) { DietaryGoalScreen(navController, newUser ) }
         composable(OnboardingScreens.WorkoutRestrictions.route) { WorkoutRestrictionsScreen(navController, newUser) }
         composable(OnboardingScreens.HeightSelection.route) {
-            Box(
+            Box (
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(Color(0xFF121212))
+                    .background(Color(0xFF091819))
             ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
+                Column(modifier = Modifier.fillMaxSize()) {
                     TopAppBar(
                         backgroundColor = Color(0xFF091819),
                         contentColor = Color.White,
-                        title = { Text("Fitness Goal (10/13)") },
-                        navigationIcon = {
-                            IconButton(onClick = { navController.popBackStack() }) {
+                        elevation = 0.dp
+                    ) {
+                        Box(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 12.dp)
+                        ) {
+                            Text(
+                                "Fitness Goal (10/13)",
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+
+                            IconButton(
+                                onClick = { navController.popBackStack() },
+                                modifier = Modifier.align(Alignment.CenterStart)
+                            ) {
                                 Icon(
                                     imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
                                     contentDescription = "Back",
                                     tint = Color(0xFFCE5100)
                                 )
                             }
-                        },
-                        actions = {
-                            IconButton(onClick = { navController.navigate(OnboardingScreens.WeightSelection.route) }) {
-                                Text("Skip", color = Color(0xFFCE5100))
+
+                            TextButton(
+                                onClick = { navController.navigate(OnboardingScreens.WeightSelection.route) },
+                                modifier = Modifier.align(Alignment.CenterEnd)
+                            ) {
+                                Text(
+                                    "Skip",
+                                    color = Color(0xFFCE5100),
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight.Medium
+                                )
                             }
                         }
-                    )
+                    }
                     HeightSelectionScreen(
                         navController,
                         OnboardingScreens.WeightSelection.route,
@@ -87,33 +109,51 @@ fun NavigateQuestions(navController: NavHostController, newUser: UserData){
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(Color(0xFF121212))
+                    .background(Color(0xFF091819))
             ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
+                Column(modifier = Modifier.fillMaxSize()) {
                     TopAppBar(
                         backgroundColor = Color(0xFF091819),
                         contentColor = Color.White,
-                        title = { Text("Fitness Goal (10/13)") },
-                        navigationIcon = {
-                            IconButton(onClick = { navController.popBackStack() }) {
+                        elevation = 0.dp
+                    ) {
+                        Box(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 12.dp)
+                        ) {
+                            Text(
+                                "Fitness Goal (11/13)",
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+
+                            IconButton(
+                                onClick = { navController.popBackStack() },
+                                modifier = Modifier.align(Alignment.CenterStart)
+                            ) {
                                 Icon(
                                     imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
                                     contentDescription = "Back",
                                     tint = Color(0xFFCE5100)
                                 )
                             }
-                        },
-                        actions = {
-                            IconButton(onClick = { navController.navigate(OnboardingScreens.AgeSelection.route) }) {
-                                Text("Skip", color = Color(0xFFCE5100))
+
+                            TextButton(
+                                onClick = { navController.navigate(OnboardingScreens.AgeSelection.route) },
+                                modifier = Modifier.align(Alignment.CenterEnd)
+                            ) {
+                                Text(
+                                    "Skip",
+                                    color = Color(0xFFCE5100),
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight.Medium
+                                )
                             }
                         }
-                    )
+                    }
                     WeightSelectionScreen(
                         navController,
                         OnboardingScreens.AgeSelection.route,
@@ -126,33 +166,51 @@ fun NavigateQuestions(navController: NavHostController, newUser: UserData){
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(Color(0xFF121212))
+                    .background(Color(0xFF091819))
             ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
+                Column(modifier = Modifier.fillMaxSize()) {
                     TopAppBar(
                         backgroundColor = Color(0xFF091819),
                         contentColor = Color.White,
-                        title = { Text("Fitness Goal (10/13)") },
-                        navigationIcon = {
-                            IconButton(onClick = { navController.popBackStack() }) {
+                        elevation = 0.dp
+                    ) {
+                        Box(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 12.dp)
+                        ) {
+                            Text(
+                                "Fitness Goal (12/13)",
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+
+                            IconButton(
+                                onClick = { navController.popBackStack() },
+                                modifier = Modifier.align(Alignment.CenterStart)
+                            ) {
                                 Icon(
                                     imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
                                     contentDescription = "Back",
                                     tint = Color(0xFFCE5100)
                                 )
                             }
-                        },
-                        actions = {
-                            IconButton(onClick = { navController.navigate(OnboardingScreens.ActivityLevel.route) }) {
-                                Text("Skip", color = Color(0xFFCE5100))
+
+                            TextButton(
+                                onClick = { navController.navigate(OnboardingScreens.ActivityLevel.route) },
+                                modifier = Modifier.align(Alignment.CenterEnd)
+                            ) {
+                                Text(
+                                    "Skip",
+                                    color = Color(0xFFCE5100),
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight.Medium
+                                )
                             }
                         }
-                    )
+                    }
                     AgeSelectionScreen(
                         navController,
                         OnboardingScreens.ActivityLevel.route,
