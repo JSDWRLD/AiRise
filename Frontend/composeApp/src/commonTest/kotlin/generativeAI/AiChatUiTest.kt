5422042eee62e0cbe25aaf615e5bd85d4ef55c92
xiangxinlin <160073499+xiangxinlin@users.noreply.github.com>
2025-11-14 13:44:11 -0800

    update: added tests to AiChatUiTests

--- Frontend/composeApp/src/commonTest/kotlin/generativeAI/AiChatUiTest.kt
+++ Frontend/composeApp/src/commonTest/kotlin/generativeAI/AiChatUiTest.kt
@@ -1,5 +1,6 @@
 package generativeAI
 
+import androidx.compose.runtime.mutableStateOf
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -30,5 +31,58 @@ class AiChatUiTest {
 
         // Assert
         assertEquals(currentScreen, "Home")
+    }
+
+    @Test
+    fun send_query_when_text_box_is_not_empty() {
+        var sendButton = false
+
+        val onSend: (String) -> Unit = {
+            if (it.isNotEmpty()) sendButton = true
+        }
+
+        onSend("")
+        assertEquals(sendButton, false)
+
+        onSend("hello")
+        assertEquals(sendButton, true)
+    }
+
+    @Test
+    fun suggested_question_chip_should_not_activate_if_used() {
+        var isSelected = false
+        var usageCount = 0
+        val maxUses = 1
+
+        val onSelect: () -> Unit = {
+            if (usageCount < maxUses) {
+                isSelected = true
+                usageCount++
+            }
+        }
+
+        onSelect()
+        assertEquals(isSelected, true)
+        assertEquals(1, usageCount)
+
+        onSelect()
+        assertEquals(isSelected, true)
+        assertEquals(1, usageCount)
+    }
+
+    @Test
+    fun addition_icon_should_bring_up_photo_upload_options() {
+        var showPhotoUploadOptions = false
+
+        val onShowPhotos: () -> Unit = {
+            showPhotoUploadOptions = true
+        }
+
+        val onClose: () -> Unit = {
+            showPhotoUploadOptions = false
+        }
+
+        onShowPhotos()
+        assertEquals(showPhotoUploadOptions, true)
     }
 }
